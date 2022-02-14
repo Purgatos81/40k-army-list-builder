645dfaab1695a0e04fe9096ed0b140653bd382c4
John HC <69842878+Purgatos81@users.noreply.github.com>
2022-02-14 16:45:11 +0100

    edit: added more objectsto unit import

--- src/App.tsx
+++ src/App.tsx
@@ -25,9 +25,31 @@ const App = () => {
     <>
       <Heading content="Thousand Sons Units"></Heading>
       <ul>
-        {data.length > 0 ? data.map((unit : {attributes : {Name : string}}, idx) => 
+        {data.length > 0 ? data.map((unit : {attributes : {Name : string,
+                                                           Move : string, 
+                                                           WeaponSkill : string,
+                                                           BallisticSkill : string,
+                                                           Strengt : string,
+                                                           Toughnes : string, 
+                                                           Attacks : string,
+                                                           Wounds : string,
+                                                           Leadership : string,
+                                                           Save : string,
+                                                           CostPerModel : string
+                                                          }}, idx) => 
             {
-              return <li key={idx}>{unit.attributes.Name}</li>;
+              return <li key={idx}><div className='unitName'>{unit.attributes.Name}</div>
+                                   <div>{unit.attributes.Move}</div>
+                                   <div>{unit.attributes.WeaponSkill}</div>
+                                   <div>{unit.attributes.BallisticSkill}</div> 
+                                   <div>{unit.attributes.Strengt}</div>
+                                   <div>{unit.attributes.Toughnes}</div>
+                                   <div>{unit.attributes.Attacks}</div>
+                                   <div>{unit.attributes.Wounds}</div>
+                                   <div>{unit.attributes.Leadership}</div>
+                                   <div>{unit.attributes.Save}</div>
+                                   <div>{unit.attributes.CostPerModel}</div>
+              </li>;
             })
           : "There are no units"}
       </ul>
